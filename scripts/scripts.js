7d5b298195506677c92109207d684e36e9e254fd
Azyn7 <113379340+Azyn7@users.noreply.github.com>
2023-09-12 15:24:48 -0500

    Updated

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,11 @@
 $('document').ready(function() {
-    $('#test p').addClass("load")
+    var i = 0;
+    $("#introduction").removeClass("fade-out");
     
+    $('#continue').click(function() {
+        if (i == 0) {
+            $("#guard").removeClass("fade-out");
+            i++;
+        }
+    });
 });
