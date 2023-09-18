98a198c97f0100ce1d7a409827c3de8f40e1059a
Ryan <113379340+Azyn7@users.noreply.github.com>
2023-09-17 19:10:43 -0500

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -84,7 +84,7 @@ function guardResponse(userName) {
                          + "that, the guard skirted off at the speed of light, leaving you to scratch your head in "
                          + "confusion. And, so, you left on your way to continue your journey. THE END.";
     // Checks if a last name is present, if so, gives you different endings.
-    } else if (lnameIndex != 0) {
+    } else if (lnameIndex > 0) {
         console.log("Test");
         if (isBetweenAandH) {
             let isBetweenAandH = (fnameSpelledOut[0].toLowerCase() >= "a" && fnameSpelledOut[0].toLowerCase() <= "h");
